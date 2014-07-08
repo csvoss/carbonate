42f4203c917426280d7324e036a1a4b162f914ad
Chelsea Voss <csvoss@mit.edu>
2014-07-08 15:48:32 -0400

    Removed api from settings.py. Added .pickle to gitignore.

--- orgo_django/orgo/settings.py
+++ orgo_django/orgo/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api',
     'orgo',
     'django.contrib.admindocs',
     'forkit',
