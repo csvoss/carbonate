c1a598b5e070d6b3d76571fd35a2d51e35292a32
Chelsea Voss <csvoss@mit.edu>
2014-07-14 14:07:32 -0400

    Admin site set up

--- orgo_django/api/admin.py
+++ orgo_django/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from api import models
 
 # Register your models here.
+admin.site.register(models.Property)
+admin.site.register(models.Reagent)
+admin.site.register(models.Reaction)
