d930c0986006151f21d0497bc7eb7bf65efec69f
vagrant <vagrant@precise32.(none)>
2014-07-18 16:47:44 +0000

    multiple choice stuff

--- orgo_django/app/views.py
+++ orgo_django/app/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 
+from random import randrange
+
+from api.models import Reagent
 from app.models import SingleStepProblem
+NUM_OPTIONS = 4
 
 # Create your views here.
 
@@ -20,6 +24,17 @@ def single_step(request, id):
     problem = SingleStepProblem.objects.get(id=id)
     context["reactantSmiles"] = problem.reactantSmiles
     context["productSmiles"] = problem.productSmiles
+    context["correctAnswer"] = problem.correctAnswer
+    
+    context["NUM_OPTIONS"] = NUM_OPTIONS
+    numReagents = len(Reagent.objects.all())
+    options = []
+    for i in range(NUM_OPTIONS - 1):
+        reagent = Reagent.objects.get(id=randrange(numReagents) + 1)
+        options.append(reagent)
+    context["incorrectAnswers"] = options
+            
+    context["incorrectAnswer"] = "wrong answer"
     return render(request, 'app/singleStep.html', context)
 
 def predict_products(request):
