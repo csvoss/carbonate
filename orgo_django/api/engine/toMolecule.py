103f80d8004316fde5361d7763f0ca583ffd96a7
Chelsea Voss <csvoss@mit.edu>
2014-07-18 17:33:36 -0400

    TODO fixes and old-code deletion
    Some, but not all, TODOs addressed. Old parser removed from this new file.

--- orgo_django/api/engine/toMolecule.py
+++ orgo_django/api/engine/toMolecule.py
@@ -48,6 +48,9 @@ def bond_at(bond, m1, a1, a2, m2):
     BASIC_BONDS = {'-':1, '=':2, '#':3, '$':4}
     if bond == '.':
         raise StandardError("bond_at called with '.' as bond")
+    elif bond == 'default':
+        ## TODO: support aromaticity!!!
+        m1.addMolecule(m2, a2, a1, 1)
     elif bond in BASIC_BONDS.keys():
         ## connect them together
         m1.addMolecule(m2, a2, a1, BASIC_BONDS[bond])
@@ -142,10 +145,7 @@ def chain_production(p):
     return Chain(m, a, a)
 @pg.production("chain : chain branched_atom")
 def chain_chain(p):
-    return chain_bond([p[0], '-', p[1]])
-    ##TODO: UNLESS end of chain and atom of branched_atom are aromatic.
-    ##Then, we need to add a 1.5 bond there, instead.
-    ##And we must be able to tell bond_at that this is the case.
+    return chain_bond([p[0], 'default', p[1]])
 @pg.production("chain : chain bond branched_atom")
 def chain_bond(p):
     bond = p[1]
@@ -244,7 +244,7 @@ class Branch(BaseBox):
 
 # branch ::= '(' chain ')' | '(' bond chain ')' | '(' dot chain ')'
 # branch :: Branch.
-@pg.production("branch : ( chain )")  ## TODO: Make ( and ) be lexable tokens in and of themselves -- as with all symbols, really
+@pg.production("branch : ( chain )")
 def branch_production(p):
     assert p[0]=='(' and p[2]==')'
     chain = p[1]
@@ -297,23 +297,21 @@ class Ringbond(BaseBox):
 # ringbond :: Ringbond
 @pg.production("ringbond : DIGIT")
 def ringbond_prod_1(p):
-    ## Again with the TODO about aromatic bonds.
-    return ringbond_prod_2(['-'] + p)
+    return ringbond_prod_2(['default'] + p)
 @pg.production("ringbond : bond DIGIT")
 def ringbond_prod_2(p):
     bond = p[0]
-    index = int(p[1])
+    index = int(p[1].getstr())
     return Ringbond(index, bond)
 @pg.production("ringbond : % DIGIT DIGIT")
 def ringbond_prod_3(p):
-    ## A third time with the TODO about aromatic bonds.
-    return ringbond_prod_4(['-'] + p)
+    return ringbond_prod_4(['default'] + p)
 @pg.production("ringbond : bond % DIGIT DIGIT")
 def ringbond_prod_4(p):
     bond = p[0]
     useless_percent = p[1]
-    index_1 = int(p[2])
-    index_2 = int(p[3])
+    index_1 = int(p[2].getstr())
+    index_2 = int(p[3].getstr())
     index = 10*index_1 + index_2
     return Ringbond(index, bond)
 
@@ -360,55 +358,8 @@ def ringbond_prod_4(p):
 
 
 
-
-
-
-
-
-
-
-
-
-################ ALL STUFF BELOW IS OLD #############################
-
-
-@pg.production("expr : expr PLUS expr")
-@pg.production("expr : expr MINUS expr")
-def expr_op(p):
-    lhs = p[0].getint()
-    rhs = p[2].getint()
-    if p[1].gettokentype() == "PLUS":
-        return BoxInt(lhs + rhs)
-    elif p[1].gettokentype() == "MINUS":
-        return BoxInt(lhs - rhs)
-    else:
-        raise AssertionError("This is impossible, abort the time machine!")
-
-@pg.production("expr : NUMBER")
-def expr_num(p):
-    return BoxInt(int(p[0].getstr()))
-
 lexer = lg.build()
 parser = pg.build()
-
-class BoxInt(BaseBox):
-    def __init__(self, value):
-        self.value = value
-
-    def getint(self):
-        return self.value
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -424,206 +375,10 @@ def moleculify(smiles):
     Raises a StandardError if the SMILES string contains
     as-yet-unsupported features (like delocalization).
     """
-
-    for i in 'cosn':
-        if i in smiles:
-            raise StandardError("Unsupported: %s" % i)
-
-    if isinstance(smiles, basestring):
-        smiles = str(smiles)
-        tokens = lexerSingle(smiles)
-        parsed = parserSingle(tokens)
-    elif type(smiles) == list:
-        tokens = map(lexerSingle, smiles)
-        parsed = map(parserSingle, tokens)
-    else:
-        raise StandardError("Invalid type of input to moleculify: input is %s, type is %s" % (repr(smiles), str(type(smiles))))
-    return parsed
-
-    ##return returnExampleMolecule().withHydrogens()
-
-def isAtomString(token):
-    """
-    token :: str. e.g. Au, [Cu+2], [C@@H]
-    return :: bool.
-    TEMPORARY -- TODO
-    """
-    if token in '-=#$/\\':
-        return False
-    return True
-
-def toAtom(token):
-    """
-    token :: str. e.g. Au, [Cu+2], [C@@H]
-    return :: Atom.
-    TEMPORARY -- TODO
-    """
-    if token[0] != '[':
-        return Atom(token)
-    else:
-        if token[2].lower() in string.lowercase:
-            return Atom(token[1:3])
-        else:
-            return Atom(token[1:2])
-
-def parserSingle(tokens):
-    """
-    tokens :: [SMILES token (str)].
-    return :: Molecule or [Molecule].
-              Will only return a list of Molecules if the SMILES is ill-formed
-              in such a way as to imply multiple molecules.
-    """
-
-    if len(tokens) == 0:
-        return None, None
-
-    first = tokens[0]
-
-    if isinstance(first, list):
-        ## This... isn't supposed to happen. But it can if the SMILES is weird.
-        ## If it does, we treat the initial branch as a *separate molecule*.
-        ## e.g. (CCCCC)N(C)Br --> CCCCC and CNBr
-        prev_mol, prev_atom = parserWithPassback(first)
-        output = [prev_mol]
-        molecule = parserSingle(tokens[1:]) ## Recursion!
-        if isinstance(molecule, list):
-            output += molecule
-        elif isinstance(molecule, Molecule):
-            output.append(molecule)
-        else:
-            raise StandardError("parserSingle returned unexpected type: %s, type %s" % (str(molecule), str(type(molecule))))
-        return output
-
-    assert isAtomString(first)
-    molecule, _ = parserWithPassback(tokens)
-
-    c40 = Atom("C")
-    c41 = Atom("C")
-    mol4 = Molecule(c40)
-    mol4.addAtom(c41, c40, 2)
-    c42 = Atom("C")
-    c43 = Atom("C")
-    mol4.addAtom(c42, c40, 1)
-    mol4.addAtom(c43, c41, 1)
-    cl1 = Atom("Cl")
-    cl2 = Atom("Cl")
-    mol4.addAtom(cl1, c40, 1)
-    mol4.addAtom(cl2, c41, 1)
-    c40.newCTCenter(c41, c42, cl1)
-    c41.newCTCenter(c40, c43, cl2)
-    mol4.addBond(c42,c43,1)
-
-    ## Comment this to actually test parserSingle
-    ## molecule = mol4
-
-    return molecule.withHydrogens()
-
-def parserWithPassback(tokens):
-    ## TODO cis trans
-    ## TODO chirality
-    ## TODO bond types
-    ## TODO ring portals
-    """
-    tokens :: [SMILES token (str)].
-    return :: ( Molecule, Atom ).
-    """
-    first = tokens.pop(0)
-    assert isAtomString(first)
-    first_atom = toAtom(first)
-
-    molecule = Molecule(first_atom)
-
-    if len(tokens) > 0:
-        next = tokens[0]
-        while isinstance(next, list):
-            next_mol, next_atom = parserWithPassback(next)
-            ## attach next_mol to molecule via next_atom connecting to first_atom
-            molecule.addMolecule(next_mol, next_atom, first_atom, 1)
-            tokens = tokens[1:]
-            next = tokens[0]
-
-        if len(tokens) > 0:
-            rest_mol, rest_atom = parserWithPassback(tokens)
-            molecule.addMolecule(rest_mol, rest_atom, first_atom, 1)
-
-    connecting_atom = first_atom
-
-    return molecule, connecting_atom
-    ## TODO: finish this method
-
-def lexerSingle(smiles):
-    """
-    smiles :: SMILES str.
-    return :: [SMILES tokens (str)].
-    """
-    output, _ = lexerWithPassback(smiles)
-    assert _ == None
-    return output
-
-def lexerWithPassback(smiles):
-    """
-    Passback: If you got to this SMILES string by taking off an open-paren,
-              this method will tokenize the stuff inside the paren, and return
-              both that list of tokens and a 'passback' -- i.e. the remainder
-              of the SMILES string.
-    smiles :: SMILES str.
-    return :: ( [SMILES tokens (str)], remaining SMILES (str) ).
-    """
-    tokens = []
-    while len(smiles) > 0:
-        prev_smiles = smiles
-        first = smiles[0]
-        if first == '[':
-            end = smiles.find(']')
-            if end == -1:
-                raise StandardError("Invalid SMILES: no matching ] in %s" % smiles)
-            tokens.append(smiles[0:end+1])
-            smiles = smiles[end+1:]
-        
-        elif first == '(':
-            subtree, remaining = lexerWithPassback(smiles[1:])
-            tokens.append(subtree)
-            smiles = remaining
-        
-        elif first in '%0123456789':
-            if first == '%':
-                num, remaining = separateFirstInts(smiles[1:])
-            else:
-                num = smiles[0]
-                remaining = smiles[1:]
-            tokens.append(num)
-            smiles = remaining
-            
-        elif first.lower() in string.lowercase:
-            ## Check if the next one is a letter too
-            try:
-                second = smiles[1]
-            except IndexError:
-                second = '$' ##any non-lowercase char
-            if second in string.lowercase:
-                tokens.append(first+second)
-                smiles = smiles[2:]
-            else:
-                tokens.append(first)
-                smiles = smiles[1:]
-        
-        elif first in '-=#$/\\':
-            tokens.append(first)
-            smiles = smiles[1:]
-
-        elif first == ')':
-            smiles = smiles[1:]
-            return tokens, smiles
-
-        ## no infinite loop!
-        assert len(smiles) < len(prev_smiles), "Infinite loop in toMolecule.lexer, unrecognized char: %s" % repr(first)
-
-    assert len(smiles) == 0, "Smiles not fully reduced in toMolecule.lexer"
-
-    return tokens, None
+    return example_molecule()
 
 
-def returnExampleMolecule():
+def example_molecule():
     """
     This method intended as a code example of how to create and return a molecule.
     This is useful to reference while writing the parser.
@@ -645,9 +400,9 @@ def returnExampleMolecule():
     c41.newCTCenter(c40, c43, cl2)
     mol4.addBond(c42,c43,1)
 
-    return mol4
+    return mol4.withHydrogens()
 
-def returnExampleSmiles():
+def example_smiles():
     """
     return :: [SMILES str].
     """
