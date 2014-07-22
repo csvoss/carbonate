35b220f52576c479863cd41e2109c6053d5c87dd
Virup GUbba <virupsg@mit.edu>
2014-07-22 16:53:29 -0400

    singlestep hard problem is on its way

--- orgo_django/app/admin.py
+++ orgo_django/app/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from app import models
 
 # Register your models here.
-admin.site.register(models.SingleStepProblem)
-admin.site.register(models.PredictProductsProblem)
+register_models = [models.SingleStepProblem, models.SingleStepHardProblem, models.PredictProductsProblem]
+
+for model in register_models:
+	admin.site.register(model)
