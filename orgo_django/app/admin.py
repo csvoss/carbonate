0de9a13c072af8a95e2aed91fc3d72a24ed18339
Chelsea Voss <csvoss@mit.edu>
2014-07-23 10:57:04 -0400

    Fancier admin.py for everything
    It now autoimports EVERYTHING. Update admin.py no longer, for it will do
    so automatically.
    
    Also, /orgo now has an admin.py.

--- orgo_django/app/admin.py
+++ orgo_django/app/admin.py
@@ -1,13 +1,21 @@
+"""
+admin.py
+Controls which models from app/models.py are accessible from 
+the admin interface.
+You can access the admin interface at localhost:4000/admin.
+"""
+
 from django.contrib import admin
+from django.db.models import Model
 from app import models
+import inspect
 
-# Register your models here.
-register_models = [
-    models.SingleStepProblem,
-    models.SingleStepHardProblem,
-    models.PredictProductsProblem,
-    models.Synthesis,
-]
-
-for model in register_models:
-	admin.site.register(model)
+## Register all the models!
+for i in dir(models):
+    thing = getattr(models, i)
+    if inspect.isclass(thing):
+        if issubclass(thing, Model):
+            try:
+                admin.site.register(thing)
+            except admin.sites.AlreadyRegistered as e:
+                pass
